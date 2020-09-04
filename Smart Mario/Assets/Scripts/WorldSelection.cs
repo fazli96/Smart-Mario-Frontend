6124821bf0512afb5300c5e2255c9189f964b949
Mohamed Fazli <moha0171@e.ntu.edu.sg>
2020-09-05 01:13:14 +0800

    Added main menu navigation

---
+++ Smart Mario/Assets/Scripts/WorldSelection.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WorldSelection : MonoBehaviour
+{
+    public void PlayWorld1()
+    {
+        SceneManager.LoadScene("World1");
+    }
+    public void PlayWorld2()
+    {
+        SceneManager.LoadScene("World2");
+    }
+}
