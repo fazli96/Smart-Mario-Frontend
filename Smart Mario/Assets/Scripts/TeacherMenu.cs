54a3246d8bb7ea8aa3fad9c74aebe8fa22a288ae
Limmy97 <Limmy97@users.noreply.github.com>
2020-10-06 11:48:41 +0800

    Updated buttons for teacher menu

--- Smart Mario/Assets/Scripts/TeacherMenu.cs
+++ Smart Mario/Assets/Scripts/TeacherMenu.cs
@@ -1,10 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TeacherMenu : MonoBehaviour
 {
     private SceneController scene;
+
+    public Button studentPerformanceButton;
+    public Button assignTaskButton;
+    public Button checkAssignedTasksButton;
+    public Button logOutButton;
+
     // Start is called before the first frame update
     void Start()
     {
