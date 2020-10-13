db72598d2d95499f6664dc38b1ad356d0894fa7c
jamesojx <48047595+jamesojx@users.noreply.github.com>
2020-10-13 21:15:33 +0800

    StudentResult

---
+++ Smart Mario/Assets/Scripts/StudentResult.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StudentResult 
+{
+    public int id;
+    public string username;
+    public string name;
+    public string password;
+    public string createdAt;
+    public string updatedAt;
+    public int fk_teacher_key;
+}
--- Smart Mario/Assets/Scripts/TaskCompletionCell.cs
+++ Smart Mario/Assets/Scripts/TaskCompletionCell.cs
@@ -1,4 +1,4 @@
-﻿susing System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
